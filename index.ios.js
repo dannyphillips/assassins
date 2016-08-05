13e4f7156d4610a89af6e6ae0e5a90e02531bddb
Ben Burlingham <ben.burlingham@procore.com>
2016-08-05 14:54:29 -0700

    Area components all in place.

--- index.ios.js
+++ index.ios.js
@@ -14,12 +14,48 @@ import {
   View
 } from 'react-native';
 
+
 import KillArea from './src/KillArea';
+import InfoArea from './src/InfoArea';
+import ListArea from './src/ListArea';
 
 class assassins extends Component {
   render() {
+    const users = [
+      {
+        name: "Danny",
+        alias: "Lucky Llama",
+        points: 100,
+        target: "James",
+        alive: 1,
+      },
+      {
+        name: "James",
+        alias: "Majestic Mountain",
+        points: 200,
+        target: "Noah",
+        alive: 1,
+      },
+      {
+        name: "Marcus",
+        alias: "Dirty Dave",
+        points: 300,
+        target: null,
+        alive: 0,
+      },
+      {
+        name: "Noah",
+        alias: "Golden Goose",
+        points: 400,
+        target: "Danny",
+        alive: 1,
+      },
+    ];
+
     return (
       <View style={styles.container}>
+        <InfoArea alias="Majestic Island" points="999" />
+        <ListArea users={users} />
         <KillArea name="Jeff Lebowski" />
       </View>
     );
--- src/ListArea.js
+++ src/ListArea.js
@@ -7,15 +7,12 @@ import {
   View,
 } from 'react-native';
 
-export default class KillArea extends React.Component {
+export default class ListArea extends React.Component {
   render() {
     return (
       <View style={styles.main}>
-        <Text style={styles.label}>REAL IDENTITY:</Text>
-        <Text style={styles.label}>{this.props.name.toUpperCase()}</Text>
-        <Text style={styles.label}>ALIAS: </Text>
-        <TextInput style={styles.textEdit} value="Placeholder" />
-        <Image style={styles.img} source={require('./images/crosshairs.png')} />
+        <Text style={styles.label}>User list</Text>
+        <Image style={styles.img} source={require('./images/trophy.png')} />
       </View>
     );
   }

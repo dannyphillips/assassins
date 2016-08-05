6487b78784b04269af07d92099a110d7f2b07b3e
Ben Burlingham <ben.burlingham@procore.com>
2016-08-05 14:17:55 -0700

    Kill Area built (waiting on style)

---
+++ KillArea.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+export default class KillArea extends React.Component {
+  render() {
+    const str = "Real identity: ";
+
+    return (
+      <View style={styles.main}>
+        <Text style={styles.label}>REAL IDENTITY:</Text>
+        <Text style={styles.label}>{this.props.name.toUpperCase()}</Text>
+        <Text style={styles.label}>ALIAS: </Text>
+        <TextInput style={styles.textEdit} value="Placeholder" />
+        <Image style={styles.crosshair} source={require('./crosshairs.png')} />
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  main: {
+    alignItems: 'flex-start',
+    alignSelf: 'stretch',
+    backgroundColor: '#444',
+    flex: 1,
+    maxHeight: 100,
+  },
+  crosshair: {
+    height: 70,
+    left: 5,
+    opacity: 0.3,
+    position: 'absolute',
+    top: 15,
+    width: 70,
+  },
+  textEdit: {
+    fontSize: 11,
+    height: 20,
+    borderColor: 'grey',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    padding: 5,
+    width: 200,
+  },
+  label: {
+    color: 'white',
+    fontSize: 11,
+    marginBottom: 10,
+    marginLeft: 80,
+    marginTop: 10,
+  },
+});
--- index.ios.js
+++ index.ios.js
@@ -7,25 +7,21 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
+
+import KillArea from './KillArea';
 
 class assassins extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <KillArea name="Jeff Lebowski" />
+        <Image style={styles.crosshair} source={require('./crosshair.svg')} />
       </View>
     );
   }
@@ -37,16 +33,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
